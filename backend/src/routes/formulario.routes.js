b650094d7ce5e2e7a7a0884c5d1fb829339ecd79
LeonardoBustosVallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2023-10-29 22:26:58 -0300

    tester 3/4 requisitos
    - sin testeo

--- backend/src/routes/formulario.routes.js
+++ backend/src/routes/formulario.routes.js
@@ -6,9 +6,13 @@ const formularioController = require("../controllers/formulario.controller.js");
 
 // Importa el middleware de autorización
 const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
-const Formulario = require("../models/formulario.model.js");
+
+// Importa el middleware de autenticación
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
+
 // Instancia del enrutador
 const router = express.Router();
+router.use(authenticationMiddleware);
 
 //Si algo lo realiza el admin, se debe agregar el middleware de autorizacion: authorizationMiddleware.isAdmin
 //Si algo lo realiza el validador, se debe agregar el middleware de autorizacion: authorizationMiddleware.isValidator
@@ -22,29 +26,3 @@ router.delete("/:id", formularioController.deleteFormularioById);
 router.get("/consulta/:email", formularioController.getEstadoFormularioByEmail);
 router.get("/consulta/:email/obs", formularioController.getObsFormularioByEmail);
 router.get("/consulta/:email/obs/:id", formularioController.getFormularioByEmailyID);
-
-"use strict";
-// Importa el modulo 'express' para crear las rutas
-const express = require("express");
-// Importa el controlador de usuarios
-const formularioController = require("../controllers/formulario.controller.js");
-
-// Importa el middleware de autorización
-const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
-
-const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
-
-// Instancia del enrutador
-const router = express.Router();
-router.use(authenticationMiddleware);
-
-//Si algo lo realiza el admin, se debe agregar el middleware de autorizacion: authorizationMiddleware.isAdmin
-//Si algo lo realiza el validador, se debe agregar el middleware de autorizacion: authorizationMiddleware.isValidator
-router.get("/",  formularioController.getFormularios);
-router.post("/", formularioController.createFormulario);
-router.get("/:id", formularioController.getFormularioById);
-router.put("/:id", authorizationMiddleware.isValidator ,formularioController.updateFormularioById);
-router.delete("/:id", formularioController.deleteFormularioById);
-router.get("/:usuario", formularioController.getEstadoFormularioByName);
-
-module.exports = router;
